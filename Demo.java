38661ea508cfc98b13dbb7499f7ca7fccdb5952c
nirbhay266 <153939270+nirbhay266@users.noreply.github.com>
2024-04-27 11:17:09 +0530

    added

--- Demo.java
+++ Demo.java
@@ -1,37 +1,45 @@
-import java.util.*;
 public class Demo
 {
-    public static void printletter(String str)
+    public static void main(String[] args)
     {
-        for(int i=0;i<str.length();i++)
+       students s1=new students();
+       s1.name="Nirbhay Yadav";
+       s1.roll=1;
+       s1.pasword="ABX";
+       s1.marks[0]=10;
+       s1.marks[1]=20;
+       s1.marks[2]=30;
+       students s2=new students(s1);
+       System.out.println(s2.name);
+       System.out.println(s2.roll);
+       System.out.println(s2.pasword);
+       s1.marks[2]=100;
+       for(int i=0;i<3;i++)
         {
-            System.out.print(str.charAt(i)+" ");
+            System.out.println(s1.marks[i]);
         }
-        System.out.println();
     }
-    public static void main(String[] args) {
-        {
-        //    String name = "Abigail";
-        //    String address = "15 Street";
-        //     System.out.println(name);
-        // Scanner ob=new Scanner(System.in);
-        // System.out.println("Enter your name");
-        // String name=ob.nextLine();
-        // System.out.println("Enter your address");
-        // String address=ob.nextLine();
-        // System.out.println(name);
-        // System.out.println(name.length());
-        // System.out.println(address);
-        // System.out.println(address.length());
-String FirstName="nirbhay";
-String lastname="Kumar";
-String FullName=FirstName+ " "+ lastname;
-
-System.out.println(FullName);
-System.out.println(FullName.length());
-System.out.println(FullName.charAt(0));
-
-printletter(FullName);
-        }
+}
+class students
+{
+    String name;
+    int roll;
+    String pasword;
+    int marks[];
+    students(students s1)
+    {
+        name=s1.name;
+        roll=s1.roll;
+        pasword=s1.pasword;
+    }
+    students()
+    {
+        marks=new int[3];
+        System.out.println("Constructor is called");
+    }
+    students(String name)
+    {
+        marks =new int[3];
+        this.name=name;
     }
 }
